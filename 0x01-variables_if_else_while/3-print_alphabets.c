6b4fb43558e5e1e0b4d28ebe4aebcb1a42ac8d80
Uju Madukwe <ujutracey@outlook.com>
2023-03-02 06:06:59 -0800

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,8 +9,9 @@ int main(void)
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i > 26; i++)
-	{putchar(alp[i]);
+	for (i = 0; i > 52; i++)
+	{
+		putchar(alp[i]);
 	}
 	putchar('/n');
 	return (0);
