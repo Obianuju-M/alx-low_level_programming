21492536e848cb284e80c21d45f93fc44c9be7a5
Uju M <ujutracey@outlook.com>
2023-03-04 05:07:12 -0800

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main-> Entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c;
+	
+	c = 48;
+	
+	while (c <= 102)
+	{
+		putchar(c);
+		
+		if (c == 57)
+			c +=39;
+		++c;
+	}
+	putchar('\n');
+	return (0);
+}
+
