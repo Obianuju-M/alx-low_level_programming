600dd3da11da99feab7ebd510599edde3b9978e1
Uju M <ujutracey@gmail.com>
2023-03-04 10:15:41 -0800

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
  *  _isalpha - Checks if xters are alphabets
- *
  * @c: xters that may be alphabets
- *
  * Return: 1 (True - if it contains alphabets) OR 0 (False - no alphabets)
  */
 
