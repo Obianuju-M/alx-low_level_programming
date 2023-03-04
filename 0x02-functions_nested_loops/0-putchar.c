15f2333047412ccb2d7fae80df556be92fb480a5
Uju M <ujutracey@outlook.com>
2023-03-04 06:54:51 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-char str[] = "_putchar";
-int i = 0;
+	char str[] = "_*putchar";
+	int i = 0;
 
-while (str[i] != '\0')
+	while (str[i] != '\0')
 	{
-	char c = str[i];
+		char c = str[i];
 
-	_putchar(c);
-	i++;
+		_putchar(c);
+		i++;
 	}
-return (0);
+	return (0);
 }
-
