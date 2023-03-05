afc3c0812e734733c14c13837b0c039531a27420
Uju M <ujutracey@outlook.com>
2023-03-05 11:09:27 -0800

    101-natural

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*add - add two int numbers and return the value
+*@a : - the first number
+*@b : - The second number to add with the add function
+* Return: void
+*/
+int add(int a, int b)
+{
+int result;
+
+result = a + b;
+return (result);
+}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main-> Entry point
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (excluded)
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c = 0;
+	int sum = 0;
+
+	while (c < 1024)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+		{
+			sum += c;
+		}
+		c++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
