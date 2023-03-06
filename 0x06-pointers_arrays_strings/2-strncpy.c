0b71bbfbc5bbf58da6e0673ae39d20f57c846606
ujutracey <ujutracey@outlook.com>
2023-03-05 18:55:21 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,21 +8,19 @@
  * @n->  bytes of @src
  *
  * Return: @dest
-*/
-
-char _strncpy(char *dest, char *src, int n)
+ */
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
 	}
-	while (i < n)
+
+	for ( ; i < n; i++)
 	{
 		dest[i] = '\0';
-		i++;
 	}
-
 	return (dest);
 }
